98beb60eed9337714dae832d18098acb3960de6a
dortmans <eric.dortmans@gmail.com>
2015-02-22 17:37:17 +0100

    Created sbs module

---
+++ sbs.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+"""sbs.py; parse Rhapsody archive file
+
+"""
+
+__author__ = "Eric Dortmans"
+__copyright__ = "Copyright 2015, Eric Dortmans"
+
+import sys, os, argparse, yaml
+
+
+def tokenize(text):
+    tokenstream = []
+    for i, line in enumerate(text):
+        line_stripped = line.strip()
+        if line_stripped != '':
+            tokenize_line(tokenstream, line_stripped)
+    return tokenstream
+
+
+def tokenize_line(tokenstream, line):
+    if line[0] == '{':
+        # object open
+        tokenstream.append('{')
+        tokenstream.append(line[1:].strip())
+    elif line[0] == '-':
+        # property
+        assignment = line[1:].split('=', 1)
+        left = assignment[0].strip()
+        tokenstream.append('=')
+        tokenstream.append(left)
+        right = assignment[1].strip()
+        if right != '':
+            if right[0] == '{':
+                tokenstream.append('{')
+                tokenstream.append(right[1:].strip())
+            else:
+                tokenstream.append(right)
+        else:
+            pass  # continued on next line
+    elif line[0] == '}':
+        # object close
+        tokenstream.append('}')
+    else:
+        if not tokenstream:
+            # first line
+            tokenstream.append(line)
+        else:
+            # remainder of previous line
+            tokenstream[-1] += line.strip(';')
+
+
+def parse(tokenstream):
+    # Parse the sbs tokenstream
+    sbs = []
+    sbs.append(parse_header(tokenstream))
+    tokenstream.pop(0)  # {
+    sbs.append(parse_object(tokenstream))
+    return sbs
+
+
+def parse_header(tokenstream):
+    token = tokenstream.pop(0)
+    tag = token[:19]
+    if tag != 'I-Logix-RPY-Archive':
+        raise Exception("ERROR: Wrong file tag: {}".format(tag))
+    return token
+
+
+def parse_object(tokenstream):
+    type = tokenstream.pop(0)
+    properties = parse_properties(tokenstream)
+    object = {type: properties}
+    return object
+
+
+def parse_properties(tokenstream):
+    # properties = []
+    properties = {}
+    token = tokenstream.pop(0)
+    while token != '}':
+        if token == '=':
+            key = tokenstream.pop(0)
+            value = parse_value(tokenstream)
+            #properties.append({key:value})
+            properties.update({key: value})
+        elif token == '{':
+            #properties.append(parse_object(tokenstream))
+            properties.update(parse_object(tokenstream))
+        else:
+            raise Exception("ERROR: Unexpected token': {}".format(token))
+        token = tokenstream.pop(0)
+    return properties
+
+
+def parse_value(tokenstream):
+    token = tokenstream.pop(0)
+    if token == '{':
+        # value is list of objects
+        value = []
+        while token == '{':
+            value.append(parse_object(tokenstream))
+            token = tokenstream.pop(0)
+        tokenstream.insert(0, token)
+        if len(value) == 1:
+            # or just one object
+            value = value[0]
+    else:
+        # or just a list of values
+        parts = token.split('"')
+        if len(parts) == 1:
+            value = [x.strip() for x in token.split(";")[:-1]]
+        else:
+            value = parts[1]
+        if len(value) == 1:
+            # or just one value
+            value = value[0]
+    return value
+
+
+def load(file):
+    try:
+        text = file.readlines()
+        tree = parse(tokenize(text))
+    except Exception, e:
+        print e
+        sys.exit(1)
+    return tree
+
+
+if __name__ == "__main__":
+    # TODO: Someday put some test here
+    pass
--- sbs_json.py
+++ sbs_json.py
@@ -1,146 +1,39 @@
 #!/usr/bin/env python
-"""sbs_json.py; Read Rhapsody archive file and write equivalent .json file
+"""sbs_json.py; Read Rhapsody archive file and write as .json file
 
-argument: filepath of a Rhapsody archive file
 """
 
 __author__ = "Eric Dortmans"
 __copyright__ = "Copyright 2015, Eric Dortmans"
 
-import os, argparse, json
+import sys, os, argparse, sbs, json
 
 
-def tokenize(text):
-    tokenstream = []
-    for i, line in enumerate(text):
-        line_stripped = line.strip()
-        if line_stripped != '':
-            tokenize_line(tokenstream, line_stripped)
-    return tokenstream
-
-
-def tokenize_line(tokenstream, line):
-    if line[0] == '{':
-         # object open
-        tokenstream.append('{')
-        tokenstream.append(line[1:].strip())
-    elif line[0] == '-':
-        # property
-        assignment = line[1:].split('=',1)
-        left = assignment[0].strip()
-        tokenstream.append('=')
-        tokenstream.append(left)
-        right = assignment[1].strip()
-        if right != '':
-            if right[0] == '{':
-                tokenstream.append('{')
-                tokenstream.append(right[1:].strip())
-            else:
-                tokenstream.append(right)
-        else:
-            pass # continued on next line
-    elif line[0] == '}':
-        # object close
-        tokenstream.append('}')
-    else:
-        if not tokenstream:
-            # first line
-            tokenstream.append(line)
-        else:
-            # remainder of previous line
-            tokenstream[-1] += line.strip(';')
-
-
-def parse(tokenstream):
-    # Parse the sbs tokenstream
-    sbs = []
-    sbs.append(parse_header(tokenstream))
-
-    if tokenstream.pop(0) == '{':
-        sbs.append(parse_object(tokenstream))
-    else:
-        raise Exception("ERROR: Unexpected token in 'parse': ",token)
-    return sbs
-
-
-def parse_header(tokenstream):
-    return tokenstream.pop(0)
-
-
-def parse_object(tokenstream):
-    type = tokenstream.pop(0)
-    properties = parse_properties(tokenstream)
-    object = {type:properties}
-    return object
-
-
-def parse_properties(tokenstream):
-    #properties = []
-    properties = {}
-    token = tokenstream.pop(0)
-    while token != '}':
-        if token == '=':
-            key = tokenstream.pop(0)
-            value = parse_value(tokenstream)
-            #properties.append({key:value})
-            properties.update({key:value})
-        elif token == '{':
-            #properties.append(parse_object(tokenstream))
-            properties.update(parse_object(tokenstream))
-        else:
-            raise Exception("ERROR: Unexpected token in 'parse_properties': ",token)
-        token = tokenstream.pop(0)
-    return properties
-
-
-def parse_value(tokenstream):
-    token = tokenstream.pop(0)
-    if token == '{':
-        # value is list of objects
-        value = []
-        while token == '{':
-            value.append(parse_object(tokenstream))
-            token = tokenstream.pop(0)
-        tokenstream.insert(0,token)
-        if len(value) == 1:
-            # or just one object
-            value = value[0]
-    else:
-        # or just a list of values
-        parts = token.split('"')
-        if len(parts) == 1:
-            value = [x.strip() for x in token.split(";")[:-1]]
-        else:
-            value = parts[1]
-        if len(value) == 1:
-            # or just one value
-            value = value[0]
-    return value
-
-def sbs_load(sbs_file):
-    text = sbs_file.readlines()
-    tokenstream = tokenize(text)
-    # print tokenstream
-    sbs = parse(tokenstream)
-    # print sbs
-    return sbs
-
-
-def sbs_to_json(sbs_file, compressed):
-    data = sbs_load(file(sbs_file, 'r'))
+def sbs_to_json(sbs_file, json_file, compressed):
+    try:
+        data = sbs.load(file(sbs_file, 'r'))
+    except IOError, e:
+        print e
+        sys.exit(1)
     if compressed:
         separators = (',', ':')
         indent = None
     else:
         separators = None
         indent = 2
-    json_file = os.path.splitext(sbs_file)[0] + '.json'
     json.dump(data, file(json_file, 'w'), separators=separators, indent=indent)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Read Rhapsody archive  file and write equivalent .json file')
+    parser = argparse.ArgumentParser(description='Read Rhapsody archive file and write equivalent .json file')
     parser.add_argument('-c', '--c', action='store_true', help='compressed json output')
-    parser.add_argument('sbs_file', help='The .sbs file to convert')
+    parser.add_argument('sbs_file', help='Rhapsody archive filepath')
+    parser.add_argument('json_file', nargs='?', help='json filepath')
     args = parser.parse_args()
-    sbs_to_json(args.sbs_file, args.c)
+    sbs_file = args.sbs_file
+    if args.json_file == None:
+        json_file = os.path.splitext(args.sbs_file)[0] + '.json'
+    else:
+        json_file = args.json_file
+
+    sbs_to_json(sbs_file, json_file, args.c)
